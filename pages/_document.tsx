9630c403abbc6caa69257f2a23c9734bf9698d68
domster4444 <kshitiz.stha11@gmail.com>
2022-02-04 11:53:51 -0500

    fix(pwa): no manifest file found,config bundlerAnalyzer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-
+        <link rel="manifest" href="/manifest.json" />
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
