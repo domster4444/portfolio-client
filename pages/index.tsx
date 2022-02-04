e82a6af36e2329dc0e9411aea213f8a690e185ab
domster4444 <kshitiz.stha11@gmail.com>
2022-02-04 06:25:05 -0500

    chore(auth0): complete setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Link from 'next/link';
-// pages/index.js
 import { useUser } from '@auth0/nextjs-auth0';
+
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 10px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
 
 const Home: NextPage = () => {
   const { user, error, isLoading } = useUser();
@@ -18,7 +34,11 @@ const Home: NextPage = () => {
     );
   }
 
-  return <Link href="/api/auth/login">Login</Link>;
+  return (
+    <Link href="/api/auth/login">
+      <PrimaryButton>Login</PrimaryButton>
+    </Link>
+  );
 };
 
 export default Home;
