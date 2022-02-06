135a6471e5a868f885aa55ac2c2c2b41a42176aa
domster4444 <kshitiz.stha11@gmail.com>
2022-02-06 05:43:08 -0500

    feat(ui): add footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 
 import { useUser } from '@auth0/nextjs-auth0';
 import { Loading } from '@nextui-org/react';
-import Navbar from '../components/Navbar';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 const Container = styled.div`
   width: 100vw;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const Block = styled.div`
@@ -46,6 +48,7 @@ const Layout: React.FC = ({ children }: any) => {
 
         {children}
       </Block>
+      <Footer />
     </Container>
   );
 };
