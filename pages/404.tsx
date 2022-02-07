162e010f4e9afde35cfc39dcb4e7e06835666b22
domster4444 <kshitiz.stha11@gmail.com>
2022-02-07 03:32:43 -0500

    Fix(vercel): cover issue while deploying due to react-transition-group type

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+import NotFoundIllustrator from 'public/images/pages/pageNotFound/PageNotFound.svg';
+import { Button } from '@nextui-org/react';
 const PageNotFound = () => {
-  return <div>page not found</div>;
+  return (
+    <>
+      <Image
+        src={NotFoundIllustrator}
+        alt="page not found"
+        width={500}
+        height={500}
+      />
+      <h1 style={{ textAlign: 'center' }}>Page not found</h1>
+      <Button color="primary" size="xl">
+        Go Back
+      </Button>
+    </>
+  );
 };
 
 export default PageNotFound;
