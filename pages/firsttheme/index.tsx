53cfcf568a845a5b8aaf570f9518ed87d7fe3534
domster4444 <kshitiz.stha11@gmail.com>
2022-02-07 11:31:38 -0500

    Fix(theme): cover responsiveness

--- pages/firsttheme/index.tsx
+++ pages/firsttheme/index.tsx
@@ -71,35 +71,42 @@ const FirstTheme = () => {
                   id="mail"
                   placeholder="Enter Your Email"
                 />
-                <input type="submit" name="submit" value="Lets Start" />
+                <input
+                  id="btn"
+                  type="submit"
+                  name="submit"
+                  value="Lets Start"
+                />
               </form>
             </div>
           </div>
 
           <div className="hero__right">
-            <Image src={MainImage} alt="sample potrait" />
+            <Image src={MainImage} alt="photograph" />
           </div>
         </div>
 
         {/* <!----About section start----> */}
         <section className="about">
           <div className="main">
-            <Image src={MainImage} alt="sample potrait" />
             <div className="about-text">
               <h2>About Me</h2>
-              <h5>
-                Developer <span>& Designer</span>
-              </h5>
-              <p>
-                I am a front-end web developer. I can provide clean code and
-                pixel perfect design. I also make the website more & more
-                interactive with web animations.I can provide clean code and
-                pixel perfect design. I also make the website more & more
-                interactive with web animations.A responsive design makes your
-                website accessible to all users, regardless of their device.
-              </p>
+              <article>
+                <h5>
+                  Developer <span>& Designer</span>
+                </h5>
+                <p>
+                  I am a front-end web developer. I can provide clean code and
+                  pixel perfect design. I also make the website more & more
+                  interactive with web animations.I can provide clean code and
+                  pixel perfect design. I also make the website more & more
+                  interactive with web animations.A responsive design makes your
+                  website accessible to all users, regardless of their device.
+                </p>
+              </article>
               <button type="button">Let's Talk</button>
             </div>
+            <Image className="about__photo" src={MainImage} alt="photograph" />
           </div>
         </section>
 
