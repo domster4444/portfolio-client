6e6d5707ecaa08b3e270ebd8af672e52d121fe90
domster4444 <kshitiz.stha11@gmail.com>
2022-02-08 08:09:30 -0500

    Fix(deploy): wrap with single children

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -6,17 +6,19 @@ import { NextPage } from 'next';
 const AboutPage: NextPage = () => {
   return (
     <Layout>
-      <Head>
-        <title>About</title>
-        <meta
-          name="description"
-          content="Meta description for the About page"
-        />
-      </Head>
+      <>
+        <Head>
+          <title>About</title>
+          <meta
+            name="description"
+            content="Meta description for the About page"
+          />
+        </Head>
 
-      <main>
-        <h1>This is about page</h1>
-      </main>
+        <main>
+          <h1>This is about page</h1>
+        </main>
+      </>
     </Layout>
   );
 };
