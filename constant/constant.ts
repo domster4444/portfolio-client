e57d2ce284b6724f64942b42ce275314cd4c08f2
domster4444 <kshitiz.stha11@gmail.com>
2022-02-10 08:16:32 -0500

    Chore(env): setup env variable

--- constant/constant.ts
+++ constant/constant.ts
@@ -2,6 +2,6 @@ interface GblI {
   readonly baseURL: string;
 }
 export const globalConstant: GblI = {
-  baseURL: 'http://localhost:3000',
-  //   baseURL: 'https://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://portfoliosite.me',
 };
