ddff53cc76df37ef692dc2a2d964af158021c379
domster4444 <kshitiz.stha11@gmail.com>
2022-02-12 02:27:23 -0500

    Chore(url): reconfigure

--- constant/constant.ts
+++ constant/constant.ts
@@ -2,6 +2,6 @@ interface GblI {
   readonly baseURL: string;
 }
 export const globalConstant: GblI = {
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://www.portfoliosite.me',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://www.portfoliosite.me',
 };
