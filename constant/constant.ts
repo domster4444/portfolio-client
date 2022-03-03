006f103e54709bdf23c1a52e6564c44356979518
domster4444 <kshitizshah@deerwalk.edu.np>
2022-03-03 10:23:37 -0500

    Fix(deploy): serverUrl not found

--- constant/constant.ts
+++ constant/constant.ts
@@ -7,7 +7,7 @@ export const globalConstant: GblI = {
   // //? auth 0 checks below base url before sending request
   //*for development
   // baseURL: 'http://localhost:3000',
-  // serverURL: 'http://localhost:5000',
+  serverURL: 'http://localhost:5000',
   //*for production
   baseURL: 'https://www.portfoliosite.me',
   //? chatserver
