aa4551fba95c4390d32295bf73232bfb801c74e1
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-03-12 09:34:00 -0500

    chore(deploy): config env

--- constant/constant.ts
+++ constant/constant.ts
@@ -6,10 +6,11 @@ interface GblI {
 export const globalConstant: GblI = {
   // //? auth 0 checks below base url before sending request
   //*for development
-  serverURL: 'http://localhost:5000',
+  // serverURL: 'http://localhost:5000',
+  serverURL: 'https://hexgrade.herokuapp.com/',
   //*for production
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://portfoliosite.me',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://portfoliosite.me',
   //? chatserver
   //*for development
   // chatServerURL: 'http://localhost:5050',
