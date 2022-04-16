5dba66992d54924bde212ed4fe83ae67339b8807
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-04-16 14:48:57 -0500

    Chore(meta): add viewport meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -102,7 +102,7 @@ class MyDocument extends Document {
         </title>
         <meta charSet="utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="view" content="width-device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width-device-width, initial-scale=1.0" />
         <meta name="author" content="Kshitiz Shah" />
         <meta
           name="description"
